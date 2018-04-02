8bce609d3a305f76e496a6c357910af3e697f8d4
Daniel Barnett <daniel.barnett@usm.edu>
2018-04-02 12:57:27 -0500

    Updated search.php to test

--- frontend/php/search.php
+++ frontend/php/search.php
@@ -68,10 +68,163 @@ if ($_POST['submit']){
 			*/
 			
 			$DSSTox_Substance_Id=$row['DSSTox_Substance_Id'];
+			$DSSTox_Structure_Id=$row['DSSTox_Structure_Id'];
+			$DSSTox_QC=$row['DSSTox_QC'];
 			$Substance_Name=$row['Substance_Name'];
+			$Substance_CASRN=$row['Substance_CASRN'];
+			$Substance_Type=$row['Substance_Type'];
+			$Substance_Note=$row['Substance_Note'];
+			$Structure_SMILES=$row['Structure_SMILES'];
+			$Structure_InChI=$row['Structure_InChI'];
+			$Structure_InChIKey=$row['Structure_InChIKey'];
+			$Structure_Formula=$row['Structure_Formula'];
+			$Structure_MolWt=$row['Structure_MolWt'];
 			
 			echo "<ul>\n";
-			echo "<li>".$DSSTox_Substance_Id." ".$Substance_Name."</li>\n";
+			echo "<li>".$DSSTox_Substance_Id." ".$DSSTox_Structure_Id." ".$DSSTox_QC." ".$Substance_Name." ".$Substance_CASRN." ".$Substance_Type." ".$Substance_Note." ".$Structure_SMILES." ".$Structure_InChI." ".$Structure_InChIKey." ".$Structure_Formula." ".$Structure_MolWt."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "tar"){
+		while ($row=mysql_fetch_array($result)){
+			$target_id=$row['target_id'];
+			$intended_target_gene_id=$row['intended_target_gene_id'];
+			$intended_target_entrez_gene_id=$row['intended_target_entrez_gene_id'];
+			$intended_target_official_full_name=$row['intended_target_official_full_name'];
+			$intended_target_gene_name=$row['intended_target_gene_name'];
+			$intended_target_official_symbol=$row['intended_target_official_symbol'];
+			$intended_target_gene_symbol=$row['intended_target_gene_symbol'];
+			$intended_target_description=$row['intended_target_description'];
+			$intended_target_uniprot_accession_number=$row['intended_target_uniprot_accession_number'];
+			$intended_target_organism_id=$row['intended_target_organism_id'];
+			$intended_target_track_status=$row['intended_target_track_status'];
+			$technological_target_gene_id=$row['technological_target_gene_id'];
+			$technological_target_entrez_gene_id=$row['technological_target_entrez_gene_id'];
+			$technological_target_official_full_name=$row['technological_target_official_full_name'];
+			$technological_target_gene_name=$row['technological_target_gene_name'];
+			$technological_target_official_symbol=$row['technological_target_official_symbol'];
+			$technological_target_gene_symbol=$row['technological_target_gene_symbol'];
+			$technological_target_description=$row['technological_target_description'];
+			$technological_target_uniprot_accession_number=$row['technological_target_uniprot_accession_number'];
+			$technological_target_organism_id=$row['technological_target_organism_id'];
+			$technological_target_track_status=$row['technological_target_track_status'];
+			
+			echo "<ul>\n";
+			echo "<li>".$target_id." ".$intended_target_gene_id." ".$intended_target_entrez_gene_id." ".$intended_target_official_full_name." ".$intended_target_gene_name." ".$intended_target_official_symbol." ".$intended_target_gene_symbol." ".$intended_target_description." ".$intended_target_uniprot_accession_number." ".$intended_target_organism_id." ".$intended_target_track_status." ".$technological_target_gene_id." ".$technological_target_entrez_gene_id." ".$technological_target_official_full_name." ".$technological_target_gene_name." ".$technological_target_official_symbol." ".$technological_target_gene_symbol." ".$technological_target_description." ".$technological_target_uniprot_accession_number." ".$technological_target_organism_id." ".$technological_target_track_status."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "tox"){
+		while($row=mysql_fetch_array($result)){
+			$SDDTox_Substance_Id=$row['SDDTox_Substance_Id'];
+			$aeid=$row['aeid'];
+			
+			echo "<ul>\n";
+			echo "<li>".$SDDTox_Substance_Id." ".$aeid."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "exp" or $type == "asy"){
+		while($row=mysql_fetch_array($result)){
+			$aid=$row['aid'];
+			$acid=$row['acid'];
+			$aeid=$row['aeid'];
+			$asid=$row['asid'];
+			$assay_source_name=$row['assay_source_name'];
+			$assay_source_long_name=$row['assay_source_long_name'];
+			$assay_source_desc=$row['assay_source_desc'];
+			$assay_name=$row['assay_name'];
+			$assay_desc=$row['assay_desc'];
+			$timepoint_hr=$row['timepoint_hr'];
+			$organism_id=$row['organism_id'];
+			$organism=$row['organism'];
+			$tissue=$row['tissue'];
+			$cell_format=$row['cell_format'];
+			$cell_free_component_source=$row['cell_free_component_source'];
+			$cell_short_name=$row['cell_short_name'];
+			$cell_growth_mode=$row['cell_growth_mode'];
+			$assay_footprINT=$row['assay_footprINT'];
+			$assay_format_type=$row['assay_format_type'];
+			$assay_format_type_sub=$row['assay_format_type_sub'];
+			$content_readout_type=$row['content_readout_type'];
+			$dilution_solvent=$row['dilution_solvent'];
+			$dilution_solvent_percent_max=$row['dilution_solvent_percent_max'];
+			$assay_component_name=$row['assay_component_name'];
+			$assay_component_desc=$row['assay_component_desc'];
+			$assay_component_target_desc=$row['assay_component_target_desc'];
+			$parameter_readout_type=$row['parameter_readout_type'];
+			$assay_design_type=$row['assay_design_type'];
+			$assay_design_type_sub=$row['assay_design_type_sub'];
+			$biological_process_target=$row['biological_process_target'];
+			$detection_technology_type=$row['detection_technology_type'];
+			$detection_technology_type_sub=$row['detection_technology_type_sub'];
+			$detection_technology=$row['detection_technology'];
+			$signal_direction_type=$row['signal_direction_type'];
+			$key_assay_reagent_type=$row['key_assay_reagent_type'];
+			$key_assay_reagent=$row['key_assay_reagent'];
+			$technological_target_type=$row['technological_target_type'];
+			$technological_target_type_sub=$row['technological_target_type_sub'];
+			$assay_component_endpoINT_name=$row['assay_component_endpoINT_name'];
+			$export_ready=$row['export_ready'];
+			$internal_ready=$row['internal_ready'];
+			$assay_component_endpoINT_desc=$row['assay_component_endpoINT_desc'];
+			$assay_function_type=$row['assay_function_type'];
+			$normalized_data_type=$row['normalized_data_type'];
+			$analysis_direction=$row['analysis_direction'];
+			$burst_assay=$row['burst_assay'];
+			$key_positive_control=$row['key_positive_control'];
+			$signal_direction=$row['signal_direction'];
+			$intended_target_type=$row['intended_target_type'];
+			$intended_target_type_sub=$row['intended_target_type_sub'];
+			$intended_target_family=$row['intended_target_family'];
+			$intended_target_family_sub=$row['intended_target_family_sub'];
+			$fit_all=$row['fit-all'];
+			$reagent_arid=$row['reagent-arid'];
+			$reagent_reagent_name_value=$row['reagent_reagent_name_value'];
+			$reagent_reagent_name_value_type=$row['reagent_reagent_name_value_type'];
+			$reagent_culture_or_assay=$row['reagent_culture_or_assay'];
+			
+			echo "<ul>\n";
+			echo "<li>".$aid." ".$acid." ".$aeid." ".$asid." ".$assay_source_name." ".$assay_source_long_name." ".$assay_source_desc." ".$assay_name." ".$assay_desc." ".$timepoint_hr." ".$organism_id." ".$organism." ".$tissue." ".$cell_format." ".$cell_free_component_source." ".$cell_short_name." ".$cell_growth_mode." ".$assay_footprINT." ".$assay_format_type." ".$assay_format_type_sub." ".$content_readout_type." ".$dilution_solvent." ".$dilution_solvent_percent_max." ".$assay_component_name." ".$assay_component_desc." ".$assay_component_target_desc." ".$parameter_readout_type." ".$assay_design_type." ".$assay_design_type_sub." ".$biological_process_target." ".$detection_technology_type." ".$detection_technology_type_sub." ".$detection_technology." ".$signal_direction_type." ".$key_assay_reagent_type." ".$key_assay_reagent." ".$technological_target_type." ".$technological_target_type_sub." ".$assay_component_endpoINT_name." ".$export_ready." ".$internal_ready." ".$assay_component_endpoINT_desc." ".$assay_function_type." ".$normalized_data_type." ".$analysis_direction." ".$burst_assay." ".$key_positive_control." ".$signal_direction." ".$intended_target_type." ".$intended_target_type_sub." ".$intended_target_family." ".$intended_target_family_sub." ".$fit_all." ".$reagent_arid." ".$reagent_reagent_name_value." ".$reagent_reagent_name_value_type." ".$reagent_culture_or_assay."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "cit"){
+		while($row=mysql_fetch_array($result)){
+			$citation_id=$row['citation_id'];
+			$pmid=$row['pmid'];
+			$doi=$row['doi'];
+			$other_source=$row['other_source'];
+			$other_id=$row['other_id'];
+			$citation=$row['citation'];
+			$title=$row['title'];
+			$author=$row['author'];
+			$url=$row['url'];
+			
+			echo "<ul>\n";
+			echo "<li>".$citation_id." ".$pmid." ".$doi." ".$other_source." ".$other_id." ".$citation." ".$title." ".$author." ".$url."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "pub"){
+		while($row=mysql_fetch_array($result)){
+			$aeid=$row['aeid'];
+			$citation_id=$row['citation_id'];
+			$pmid=$row['pmid'];
+			
+			echo "<ul>\n";
+			echo "<li>".$aeid." ".$citation_id." ".$pmid."</li>\n";
+			echo "</ul>";
+		}
+	}
+	elseif ($type == "tes"){
+		while($row=mysql_fetch_array($result)){
+			$aeid=$row['aeid'];
+			$target_id=$row['target_id'];
+			$DSSTox_Substance_Id=$row['DSSTox_Substance_Id'];
+			
+			echo "<ul>\n";
+			echo "<li>".$aeid." ".$target_id." ".$DSSTox_Substance_Id."</li>\n";
 			echo "</ul>";
 		}
 	}
