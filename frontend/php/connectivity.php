d566ceda5dba176a7c86da8bf1acf810e9fae4a8
dbarn767 <daniel.barnett@eagles.usm.edu>
2018-04-19 22:37:12 +0000

    Add files via upload

--- frontend/php/connectivity.php
+++ frontend/php/connectivity.php
@@ -1,15 +1,43 @@
 <?php
 	// DEBUG OUTPUT
 	echo "connectivity.php should be running! \n";
+	// TURNING ON ERROR REPORTING
+	ini_set('display_errors', 'On');
 	// Variables
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
+	$username = "chem";
+	$password = "KCGriffith";
 	$databasename = "ChemDB";
 	// Create connection
 	$mysqli = new mysqli($servername, $username, $password, $databasename);
+	//$conn = mysql_connect($servername, $username);
+	/*
+	$conn = new mysqli($servername, $username, $password);
+	mysqli_select_db($databasename, $conn) or die (mysql_error());
+	*/
+	//$conn = new mysqli($servername, $username, $password, $databasename);
 	// Check connection
 	if(!$mysqli) {
+	//if(!$conn)
 		die("connection failed: " . mysqli_connect_error());
+	}
+	/*
+	else
+	{
+		echo "pre run\n";
+		echo $mysqli->query("SHOW TABLES");
+		echo "\npost run";
+	}
+	*/
+
+	if (mysqli_ping($mysqli)){
+	//if (mysqli_ping($conn)){
+		echo "Our connection is ok!\n";
+		//printf("Our connection is ok!\n");
+	} else {
+		echo "ERROR: connection is NOT ok!\n";
+		echo "ERROR MESSAGE: ".mysqli_error($mysqli);
+		//echo "ERROR MESSAGE: ".mysqli_error($conn);
+		//printf("Error: %s\n", mysqli_error($mysqli));
 	}
 ?>
--- frontend/php/search.php
+++ frontend/php/search.php
@@ -1,11 +1,8 @@
 <?php 
 include 'connectivity.php';
-
 // REFERENCE USED: webreference.com/programming/php/search/2.html
-
 // DEBUG OUTPUT
 echo "search.php should be running! \n";
-
 // check for submission
 // retrieve value from posted data
 if ($_POST['submit']){
@@ -20,28 +17,96 @@ if ($_POST['submit']){
 	$id = $_POST['id'];
 	$symbol = $_POST['symbol'];
 	$type = $_POST['item-type'];
-	//$query;
+
+	// SET default values
+	if ($name == "" || $name == " "){
+		$name = NULL;
+	}
+	if ($id == "" || $id == " "){
+		$id = NULL;
+	}
+	if ($symbol == "" || $symbol == " "){
+		$symbol = NULL;
+	}
 	
 	// Create the query
 	if ($type == "che"){
 		//$query = "SELECT cid, casn, FROM Chemicals WHERE cid LIKE '%".$id."%' OR casn LIKE'%".$name."%'";
-		$query = "SELECT DSSTox_Substance_Id, Substance_Name FROM Chemicals WHERE DSSTox_Substance_Id LIKE '%".$id."%' OR Substance_Name LIKE '%".$name."%'";
+		//$query = "SELECT DSSTox_Substance_Id, Substance_Name FROM Chemicals WHERE DSSTox_Substance_Id LIKE '%".$id."%' OR Substance_Name LIKE '%".$name."%'";
+		//$query = "SELECT * FROM Chemicals";
+		//$query = "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Substance_Name LIKE '%".$name."%' OR Substance_CASRN LIKE '%".$id."%' OR Structure_SMILES LIKE '%".$symbol."%'";
+		//if ($name != NULL || $id != NULL || $symbol != NULL){
+			/*
+			$query = "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Substance_Name LIKE '%".$name."%' 
+			AND EXISTS ( 
+			SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Substance_CASRN LIKE '%".$id."%' 
+			) AND EXISTS ( 
+			SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Structure_SMILES LIKE '%".$symbol."%')";
+			*/
+
+		$query = "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals";
+		$original = $query;
+		$partCounter = 0;
+		if ($name != NULL){
+			if ($query == $original) {
+				$query = "";
+			}
+			$combiner = "";
+			if($partCounter >= 1){
+				$combiner = " AND EXISTS (";
+			}
+			$query = $query . $combiner . "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Substance_Name LIKE '%".$name."%'";
+			//$combineFlag = true;
+			$partCounter += 1;
+		}
+		if ($id != NULL){
+			if ($query == $original) {
+				$query = "";
+			}
+			$combiner = "";
+			if ($partCounter >= 1){
+				$combiner = " AND EXISTS (";
+			}
+
+			$query = $query . $combiner . "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Substance_CASRN LIKE '%".$id."%'";
+			$partCounter += 1;
+		}
+		if ($symbol != NULL){
+			if ($query == $original) {
+				$query = "";
+			}
+			$combiner = "";
+			if ($partCounter >= 1){
+				$combiner = " AND EXISTS (";
+			}
+
+			$query = $query . $combiner . "SELECT Substance_Name, Substance_CASRN, Structure_SMILES, Structure_InChI, Structure_Formula, Structure_MolWt FROM Chemicals WHERE Structure_SMILES LIKE '%".$symbol."%'";
+			$partCounter += 1;
+		}
+		while ($partCounter - 1 > 0){
+			$query = $query . ")";
+			$partCounter -= 1;
+		}
+		
+		echo "\n----------------------------------------\n";
+		echo $query;
+		echo  "\n----------------------------------------\n";
 	}
 	elseif ($type == "tox"){
 		$query = "SELECT * FROM Toxicity";
 	}
 	elseif ($type == "tar"){
-		$query = "SELECT target_id FROM Target WHERE target_id LIKE '%".$id."%'";
+		//$query = "SELECT * FROM Target";
+		//$query = "SELECT target_id FROM Target WHERE target_id LIKE '%".$id."%'";
+		$query = "SELECT target_id, intended_target_official_full_name, intended_target_gene_name, intended_target_official_symbol, intended_target_gene_symbol, technological_target_official_full_name, technological_target_gene_name, technological_target_official_symbol, technological_target_gene_symbol FROM Target WHERE target_id LIKE '%".$id."%' OR intended_target_official_full_name LIKE '%".$name."%'";
 	}
-	elseif ($type == "exp" or $type == "asy"){
-		//$query = "SELECT aeid FROM Experiments WHERE aeid LIKE '%".$id."%'";
-		$query = "SELECT aeid FROM Assay WHERE aeid LIKE '%".$id."%'";
+	elseif ($type == "asy"){
+		//$query = "SELECT aeid FROM Assay WHERE aeid LIKE '%".$id."%'";
+		$query = "SELECT * FROM Assay";
 	}
-	//elseif ($_POST['item-type'] == "asy"){
-	//	$query = "
-	//}
 	elseif ($type == "cit"){
-		$query = "SELECT citation_id FROM Citation WHERE citation_id LIKE '%".$id."%'";
+		//$query = "SELECT citation_id FROM Citation WHERE citation_id LIKE '%".$id."%'";
+		$query = "SELECT * FROM Citation";
 	}
 	elseif ($type == "pub"){
 		$query = "SELECT * FROM Published";
@@ -52,70 +117,81 @@ if ($_POST['submit']){
 	else{
 		echo "ERROR: No table specified! \n";
 	}
+
+	// DEBUG
+	echo "DEBUG: query to use was determined to be ".$query."!";
+	echo "DEBUG: Checking our connection!";
 	
+	if (!$mysqli)
+	//if (!$conn)
+	{
+		echo "ERROR:No connection before getting result in search.php!";
+	}
+	
+	if (mysqli_ping($mysqli)){
+	//if (mysqli_ping($conn)){
+		echo "Our connection is ok!\n";
+	} else {
+		echo "ERROR: connection is NOT ok!\n";
+		echo "ERROR MESSAGE: ".mysqli_error($mysqli);
+		//echo "ERROR MESSAGE: ".mysqli_error($conn);
+	}
+
+	echo "DEBUG: About to get result!";
 	// Get a result
-	$result = mysql_query($query);
+	//$result = mysql_query($query);
+	//$result = mysqli_query($mysqli, $query); // Continues on
+	$result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli)); // Sudden stop in code... why?
+	//$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+	$numOfRows=mysqli_num_rows($result);
+	//if ($result->num_rows === 0)
+	if ($numOfRows == 0)
+	{
+		echo "--NO RESULTS FOUND!--";
+	}
+	else
+	{
+		echo "Result returned ".$numOfRows." number of rows!";
+	}
+
+	// DEBUG
+	echo "DEBUG: About to try to print result based on type!";
 	
 	// Create while loop and loop through result set
 	if ($type == "che"){
-		while ($row=mysql_fetch_array($result)){
-			/*
-			$cid=$row['cid'];
-			$casn=$row['casn'];
+		while ($row=mysqli_fetch_array($result)){
 			
-			echo "<ul>\n";
-			echo "<li>".$cid." ".$casn."</li>\n
-			*/
-			
-			$DSSTox_Substance_Id=$row['DSSTox_Substance_Id'];
-			$DSSTox_Structure_Id=$row['DSSTox_Structure_Id'];
-			$DSSTox_QC=$row['DSSTox_QC'];
 			$Substance_Name=$row['Substance_Name'];
 			$Substance_CASRN=$row['Substance_CASRN'];
-			$Substance_Type=$row['Substance_Type'];
-			$Substance_Note=$row['Substance_Note'];
 			$Structure_SMILES=$row['Structure_SMILES'];
 			$Structure_InChI=$row['Structure_InChI'];
-			$Structure_InChIKey=$row['Structure_InChIKey'];
 			$Structure_Formula=$row['Structure_Formula'];
 			$Structure_MolWt=$row['Structure_MolWt'];
 			
 			echo "<ul>\n";
-			echo "<li>".$DSSTox_Substance_Id." ".$DSSTox_Structure_Id." ".$DSSTox_QC." ".$Substance_Name." ".$Substance_CASRN." ".$Substance_Type." ".$Substance_Note." ".$Structure_SMILES." ".$Structure_InChI." ".$Structure_InChIKey." ".$Structure_Formula." ".$Structure_MolWt."</li>\n";
+			echo "<li>".$Substance_Name." ".$Substance_CASRN." ".$Structure_SMILES." ".$Structure_InChI." ".$Structure_Formula." ".$Structure_MolWt."</li>\n";
 			echo "</ul>";
 		}
 	}
 	elseif ($type == "tar"){
-		while ($row=mysql_fetch_array($result)){
-			$target_id=$row['target_id'];
-			$intended_target_gene_id=$row['intended_target_gene_id'];
-			$intended_target_entrez_gene_id=$row['intended_target_entrez_gene_id'];
+		while ($row=mysqli_fetch_array($result)){
 			$intended_target_official_full_name=$row['intended_target_official_full_name'];
 			$intended_target_gene_name=$row['intended_target_gene_name'];
 			$intended_target_official_symbol=$row['intended_target_official_symbol'];
 			$intended_target_gene_symbol=$row['intended_target_gene_symbol'];
-			$intended_target_description=$row['intended_target_description'];
-			$intended_target_uniprot_accession_number=$row['intended_target_uniprot_accession_number'];
-			$intended_target_organism_id=$row['intended_target_organism_id'];
-			$intended_target_track_status=$row['intended_target_track_status'];
-			$technological_target_gene_id=$row['technological_target_gene_id'];
-			$technological_target_entrez_gene_id=$row['technological_target_entrez_gene_id'];
 			$technological_target_official_full_name=$row['technological_target_official_full_name'];
 			$technological_target_gene_name=$row['technological_target_gene_name'];
 			$technological_target_official_symbol=$row['technological_target_official_symbol'];
 			$technological_target_gene_symbol=$row['technological_target_gene_symbol'];
-			$technological_target_description=$row['technological_target_description'];
-			$technological_target_uniprot_accession_number=$row['technological_target_uniprot_accession_number'];
-			$technological_target_organism_id=$row['technological_target_organism_id'];
-			$technological_target_track_status=$row['technological_target_track_status'];
-			
+
 			echo "<ul>\n";
-			echo "<li>".$target_id." ".$intended_target_gene_id." ".$intended_target_entrez_gene_id." ".$intended_target_official_full_name." ".$intended_target_gene_name." ".$intended_target_official_symbol." ".$intended_target_gene_symbol." ".$intended_target_description." ".$intended_target_uniprot_accession_number." ".$intended_target_organism_id." ".$intended_target_track_status." ".$technological_target_gene_id." ".$technological_target_entrez_gene_id." ".$technological_target_official_full_name." ".$technological_target_gene_name." ".$technological_target_official_symbol." ".$technological_target_gene_symbol." ".$technological_target_description." ".$technological_target_uniprot_accession_number." ".$technological_target_organism_id." ".$technological_target_track_status."</li>\n";
+			echo "<li>".$intended_target_official_full_name." | ".$intended_target_gene_name." | ".$intended_target_official_symbol." | ".$intended_target_gene_symbol." | ".$technological_target_official_full_name." | ".$technological_target_gene_name." | ".$technological_target_official_symbol." | ".$technological_target_gene_symbol."</li>\n";
 			echo "</ul>";
 		}
 	}
 	elseif ($type == "tox"){
-		while($row=mysql_fetch_array($result)){
+		while($row=mysqli_fetch_array($result)){
 			$SDDTox_Substance_Id=$row['SDDTox_Substance_Id'];
 			$aeid=$row['aeid'];
 			
@@ -124,8 +200,13 @@ if ($_POST['submit']){
 			echo "</ul>";
 		}
 	}
-	elseif ($type == "exp" or $type == "asy"){
-		while($row=mysql_fetch_array($result)){
+	//elseif ($type == "exp" or $type == "asy"){
+	elseif ($type == "asy"){
+		//DEBUG
+		echo "DEBUG: About to start while loop for Assay!";
+		//while($row=mysql_fetch_array($result)){
+		while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+			echo "DEBUG: Inside while loop for Assay!";
 			$aid=$row['aid'];
 			$acid=$row['acid'];
 			$aeid=$row['aeid'];
@@ -184,13 +265,19 @@ if ($_POST['submit']){
 			$reagent_reagent_name_value_type=$row['reagent_reagent_name_value_type'];
 			$reagent_culture_or_assay=$row['reagent_culture_or_assay'];
 			
+			// DEBUGGING
+			echo "DEBUG: ABOUT TO PRINT ASSAY TABLE!";
+			echo "'%".$aid."%'";
+			echo "".$aid." ".$acid." ".$aeid." ".$asid." ".$assay_source_name." ".$assay_source_long_name." ".$assay_source_desc." ".$assay_name." ".$assay_desc." ".$timepoint_hr." ".$organism_id." ".$organism." ".$tissue." ".$cell_format." ".$cell_free_component_source." ".$cell_short_name." ".$cell_growth_mode." ".$assay_footprINT." ".$assay_format_type." ".$assay_format_type_sub." ".$content_readout_type." ".$dilution_solvent." ".$dilution_solvent_percent_max." ".$assay_component_name." ".$assay_component_desc." ".$assay_component_target_desc." ".$parameter_readout_type." ".$assay_design_type." ".$assay_design_type_sub." ".$biological_process_target." ".$detection_technology_type." ".$detection_technology_type_sub." ".$detection_technology." ".$signal_direction_type." ".$key_assay_reagent_type." ".$key_assay_reagent." ".$technological_target_type." ".$technological_target_type_sub." ".$assay_component_endpoINT_name." ".$export_ready." ".$internal_ready." ".$assay_component_endpoINT_desc." ".$assay_function_type." ".$normalized_data_type." ".$analysis_direction." ".$burst_assay." ".$key_positive_control." ".$signal_direction." ".$intended_target_type." ".$intended_target_type_sub." ".$intended_target_family." ".$intended_target_family_sub." ".$fit_all." ".$reagent_arid." ".$reagent_reagent_name_value." ".$reagent_reagent_name_value_type." ".$reagent_culture_or_assay."</li>\n";
+			
+
 			echo "<ul>\n";
 			echo "<li>".$aid." ".$acid." ".$aeid." ".$asid." ".$assay_source_name." ".$assay_source_long_name." ".$assay_source_desc." ".$assay_name." ".$assay_desc." ".$timepoint_hr." ".$organism_id." ".$organism." ".$tissue." ".$cell_format." ".$cell_free_component_source." ".$cell_short_name." ".$cell_growth_mode." ".$assay_footprINT." ".$assay_format_type." ".$assay_format_type_sub." ".$content_readout_type." ".$dilution_solvent." ".$dilution_solvent_percent_max." ".$assay_component_name." ".$assay_component_desc." ".$assay_component_target_desc." ".$parameter_readout_type." ".$assay_design_type." ".$assay_design_type_sub." ".$biological_process_target." ".$detection_technology_type." ".$detection_technology_type_sub." ".$detection_technology." ".$signal_direction_type." ".$key_assay_reagent_type." ".$key_assay_reagent." ".$technological_target_type." ".$technological_target_type_sub." ".$assay_component_endpoINT_name." ".$export_ready." ".$internal_ready." ".$assay_component_endpoINT_desc." ".$assay_function_type." ".$normalized_data_type." ".$analysis_direction." ".$burst_assay." ".$key_positive_control." ".$signal_direction." ".$intended_target_type." ".$intended_target_type_sub." ".$intended_target_family." ".$intended_target_family_sub." ".$fit_all." ".$reagent_arid." ".$reagent_reagent_name_value." ".$reagent_reagent_name_value_type." ".$reagent_culture_or_assay."</li>\n";
 			echo "</ul>";
 		}
 	}
 	elseif ($type == "cit"){
-		while($row=mysql_fetch_array($result)){
+		while($row=mysqli_fetch_array($result)){
 			$citation_id=$row['citation_id'];
 			$pmid=$row['pmid'];
 			$doi=$row['doi'];
@@ -207,7 +294,7 @@ if ($_POST['submit']){
 		}
 	}
 	elseif ($type == "pub"){
-		while($row=mysql_fetch_array($result)){
+		while($row=mysqli_fetch_array($result)){
 			$aeid=$row['aeid'];
 			$citation_id=$row['citation_id'];
 			$pmid=$row['pmid'];
@@ -218,7 +305,7 @@ if ($_POST['submit']){
 		}
 	}
 	elseif ($type == "tes"){
-		while($row=mysql_fetch_array($result)){
+		while($row=mysqli_fetch_array($result)){
 			$aeid=$row['aeid'];
 			$target_id=$row['target_id'];
 			$DSSTox_Substance_Id=$row['DSSTox_Substance_Id'];
@@ -231,31 +318,7 @@ if ($_POST['submit']){
 	
 }
 
-/* 
-	//Search Query
-	$query = "(SELECTED cid, casn as type FROM chemicals WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED aeid as type FROM experiments WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED * as type FROM toxicity WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED * as type FROM tested WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED * as type FROM published WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED citation_id as type FROM citations WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')
-		UNION
-		(SELECTED target_id as type FROM targets WHERE content LIKE '%" . $keyword . "%' OR title LIKE '%" . $keyword . "%')";
-
-
-	$result = mysqli_query($conn, $query);
-
-	if(mysqli_num_rows($result) > 0){
-		// output data of each row
-		while($row = mysqli_fetch_assoc($result)){
-			echo $result
-		}
-	}
-*/
-
+echo "DEBUG: Ending search.php";
+mysqli_close($mysqli);
+//mysqli_close($conn);
 ?>
